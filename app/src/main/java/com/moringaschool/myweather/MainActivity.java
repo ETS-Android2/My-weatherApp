90490f9e865f02f57ba11ecd99bd29115fa83f66
diana3664 <dianamus2017@gmail.com>
2021-07-27 08:45:15 +0300

    create an intent to on the sign up button to navigate to the sign up activity

--- app/src/main/java/com/moringaschool/myweather/MainActivity.java
+++ app/src/main/java/com/moringaschool/myweather/MainActivity.java
@@ -2,13 +2,43 @@ package com.moringaschool.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //binds for user when they want to create an account
+    @BindView(R.id.LoginInButton) Button mLoginButton;
+    @BindView(R.id.signUpButton) TextView mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //set the buutton on click
+        mLoginButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
+    }
+
+    //intents when a user clicks the buttons
+    @Override
+    public void onClick(View v) {
+
+        if(v==mSignUpButton){
+//            navigate to sign up activity
+            Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if(v==mLoginButton){
+            //navigate to login activity
+        }
     }
 }
