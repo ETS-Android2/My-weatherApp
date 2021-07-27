04748f709c79657e1bf4337909f5cc8f1a02c0d3
diana3664 <dianamus2017@gmail.com>
2021-07-27 09:43:50 +0300

    create an intent in the Login when a user does not have an account

--- app/src/main/java/com/moringaschool/myweather/LoginActivity.java
+++ app/src/main/java/com/moringaschool/myweather/LoginActivity.java
@@ -2,13 +2,37 @@ package com.moringaschool.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    //signActivity TextView
+    @BindView(R.id.registerTextView) TextView mRegesterText; //for navigating to signup
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mRegesterText.setOnClickListener(this);//for signup TextView
+    }
+
+
+    //set view on click for navigating to the signup button
+    @Override
+    public void onClick(View v) {
+        if(v==mRegesterText){
+            //navigate to sinup activity
+            Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
