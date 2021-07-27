717d4e431f1d1ca26a561fee50688ea2f47fe284
diana3664 <dianamus2017@gmail.com>
2021-07-27 09:55:00 +0300

    set a click listener on loginTextView on SignUpActivity for user to navigate to Login if they already have an account

--- app/src/main/java/com/moringaschool/myweather/SignUpActivity.java
+++ app/src/main/java/com/moringaschool/myweather/SignUpActivity.java
@@ -2,13 +2,37 @@ package com.moringaschool.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //bind the Login Text view
+    @BindView(R.id.loginTextView) TextView mAlreadyHaveAccount;//when a user aleady has an account
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        //set TextView on click listener
+        mAlreadyHaveAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //navigate to Login Page on click
+        if(mAlreadyHaveAccount == v){
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
