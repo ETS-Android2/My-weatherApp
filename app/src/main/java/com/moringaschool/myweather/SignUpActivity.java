4331e61382d4fd24b80d2bb7acd2dd0ff835a85e
diana3664 <dianamus2017@gmail.com>
2021-07-27 10:41:54 +0300

    Connect to firebase and CreateNew user to firebase

--- app/src/main/java/com/moringaschool/myweather/SignUpActivity.java
+++ app/src/main/java/com/moringaschool/myweather/SignUpActivity.java
@@ -1,19 +1,35 @@
 package com.moringaschool.myweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
-
+    public static final String TAG = SignUpActivity.class.getSimpleName();
     //bind the Login Text view
     @BindView(R.id.loginTextView) TextView mAlreadyHaveAccount;//when a user aleady has an account
+    //create account in Firebase
+    @BindView(R.id.createUserButton) Button mCreateAccount; //button
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPassord; 
+    @BindView(R.id.passwordEditText) EditText mPassordEditText;
+    @BindView(R.id.emailEditText)EditText mEmailEditText;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    //add member variable to get instance
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +38,10 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         ButterKnife.bind(this);
         //set TextView on click listener
         mAlreadyHaveAccount.setOnClickListener(this);
+        //set Button on click listener
+        mCreateAccount.setOnClickListener(this);
+        //get instance of firebase
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -33,6 +53,26 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(intent);
             finish();
         }
+        if(mCreateAccount == v){
+            createNewUser();
+        }
 
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPassordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPassord.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this,task -> {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "Authentication successful");
+                    }else {
+                        Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
