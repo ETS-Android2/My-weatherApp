e3585ed1c7df23817dc17757eed9c6f1ec646fe2
diana3664 <dianamus2017@gmail.com>
2021-07-28 11:40:31 +0300

    add internet permisions in android manifect

---
+++ app/src/main/java/com/moringaschool/myweather/Constants.java
@@ -0,0 +1,8 @@
+package com.moringaschool.myweather;
+
+
+public class Constants {
+
+    public static final String WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+    public static final String WEATHER_API_KEY = "439d4b804bc8187953eb36d2a8c26a02";
+}
